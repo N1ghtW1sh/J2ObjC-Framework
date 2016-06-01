dbca7d18d47087dd5ff60ceab8a0542305e4ab53
N1ghtW1sh <116516842@qq.com>
2016-06-01 10:25:25 +0800

    Update download.sh
    fix url

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -5,7 +5,7 @@ j2objc_version=1.0.2
 sha1_checksum=6f2f45b323ef92cc8d1b13dd789aebd1ab87562f
 
 echo "fetching j2objc dist2"
-curl -OL https://localhost/j2objc-${j2objc_version}.zip
+curl -OL http://localhost/j2objc-${j2objc_version}.zip
 unzip -o -q j2objc-${j2objc_version}.zip
 mv j2objc-${j2objc_version} Distributive
 rm j2objc-${j2objc_version}.zip
