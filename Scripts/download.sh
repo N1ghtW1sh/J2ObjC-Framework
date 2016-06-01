93df94656e13040d6e4bed0884ae7996145f4f71
N1ghtW1sh <116516842@qq.com>
2016-06-01 10:23:00 +0800

    Update download.sh
    123

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -6,7 +6,6 @@ sha1_checksum=6f2f45b323ef92cc8d1b13dd789aebd1ab87562f
 
 echo "fetching j2objc dist2"
 curl -OL https://localhost/j2objc-${j2objc_version}.zip
-echo "${sha1_checksum}  j2objc-${j2objc_version}.zip" | shasum -c
 unzip -o -q j2objc-${j2objc_version}.zip
 mv j2objc-${j2objc_version} Distributive
 rm j2objc-${j2objc_version}.zip
